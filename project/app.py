25db0bd5627e76410cbdccd2699eb884e7bc8cf5
yanisakham <yanisagreen@gmail.com>
2020-10-27 16:43:59 -0400

    change to postgres db

--- project/app.py
+++ project/app.py
@@ -5,6 +5,7 @@ from functools import wraps
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 basedir = Path(__file__).resolve().parent
@@ -14,7 +15,10 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    'DATABASE_URL',
+    f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
